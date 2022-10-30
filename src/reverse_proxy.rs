6406ef8a3907fcff3b87399924f4053659719a78
Dillen Meijboom <info@excol.nl>
2022-10-30 22:01:11 +0100

    fix: implement a workaround for HTTP2 graceful error in hyper

--- src/reverse_proxy.rs
+++ src/reverse_proxy.rs
@@ -1,4 +1,5 @@
 use std::{
+    error::Error,
     fmt::{self, Display},
     str::FromStr,
 };
@@ -13,6 +14,7 @@ use warp::{
     Rejection,
 };
 
+const MAX_RETRIES: usize = 3;
 const HOP_HEADERS: [&str; 8] = [
     "Connection",
     "Keep-Alive",
@@ -62,41 +64,56 @@ impl Request {
             body,
         }
     }
+}
 
-    pub fn into_request(self, upstream: &str) -> Result<hyper::Request<Body>, Rejection> {
-        let url = format!("{}{}?{}", upstream, self.path.as_str(), self.query);
-        let url = Uri::from_str(&url).map_err(IntoRequestError::InvalidUri)?;
+fn create_proxied_request(
+    req: &Request,
+    upstream: &str,
+) -> Result<hyper::Request<Body>, Rejection> {
+    let url = format!("{}{}?{}", upstream, req.path.as_str(), req.query);
+    let url = Uri::from_str(&url).map_err(IntoRequestError::InvalidUri)?;
 
-        let mut out = hyper::Request::new(if self.method == Method::GET {
-            Body::empty()
-        } else {
-            Body::from(self.body)
-        });
+    let mut out = hyper::Request::new(req.body.clone().into());
 
-        *out.method_mut() = self.method;
-        *out.uri_mut() = url;
+    *out.method_mut() = req.method.clone();
+    *out.uri_mut() = url;
 
-        let headers = out.headers_mut();
-        copy_headers(self.headers, headers);
+    let headers = out.headers_mut();
+    copy_headers(req.headers.clone(), headers);
 
-        // Remove the host header as it will be set automatically
-        headers.remove("host");
+    // Remove the host header as it will be set automatically
+    headers.remove("host");
 
-        log::trace!("proxy request (request={:#?})", out);
+    log::trace!("proxy request (request={:#?})", out);
 
-        Ok(out)
-    }
+    Ok(out)
 }
 
 #[derive(Debug)]
-pub struct HyperError(hyper::Error);
+pub enum ProxyError {
+    Hyper(hyper::Error),
+    MaxRetries(hyper::Error),
+}
 
-impl Reject for HyperError {}
+impl Reject for ProxyError {}
 
 impl Display for Request {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{} /{}", self.method, self.path.as_str())
     }
+}
+
+#[inline]
+fn is_h2_goaway_no_error(e: &hyper::Error) -> bool {
+    if let Some(source) = e.source() {
+        if let Some(e) = source.downcast_ref::<h2::Error>() {
+            if e.is_go_away() && e.is_remote() && e.reason() == Some(h2::Reason::NO_ERROR) {
+                return true;
+            }
+        }
+    }
+
+    false
 }
 
 pub async fn handle<C>(
@@ -107,15 +124,35 @@ pub async fn handle<C>(
 where
     C: Connect + Clone + Send + Sync + 'static,
 {
-    let request = request.into_request(upstream)?;
-    let mut response = http
-        .request(request)
-        .await
-        .map_err(|e| reject::custom(HyperError(e)))?;
+    let mut retries = MAX_RETRIES;
 
-    log::trace!("proxy response (response={:#?})", response);
+    loop {
+        let request = create_proxied_request(&request, upstream)?;
 
-    remove_hop_headers(response.headers_mut());
+        match http.request(request).await {
+            Ok(mut response) => {
+                log::trace!("proxy response (response={:#?})", response);
 
-    Ok(response)
+                remove_hop_headers(response.headers_mut());
+
+                return Ok(response);
+            }
+            Err(e) => {
+                // fixes: https://github.com/hyperium/hyper/issues/2500
+                if is_h2_goaway_no_error(&e) {
+                    retries -= 1;
+
+                    if retries == 0 {
+                        return Err(reject::custom(ProxyError::MaxRetries(e)));
+                    }
+
+                    log::debug!("retrying request");
+
+                    continue;
+                }
+
+                return Err(reject::custom(ProxyError::Hyper(e)));
+            }
+        }
+    }
 }
